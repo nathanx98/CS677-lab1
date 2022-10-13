55be9d62c9037cea381a486a974ebf18ac3eab19
nathanx98 <xuanjiyang09@gmail.com>
2022-10-13 17:19:21 -0400

    .

---
+++ server.py
@@ -0,0 +1,13 @@
+from rpyc import Service
+from rpyc.utils.server import ThreadedServer
+
+import time
+
+class TimeService(Service):
+	def exposed_get_time(self):
+		return time.ctime()
+
+
+if __name__ == '__main__':
+    s = ThreadedServer(TimeService, port=18871)
+    s.start()
