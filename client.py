c1d95c0c10b3965f72bd8cf726c6dd468db7e606
nathanx98 <xuanjiyang09@gmail.com>
2022-10-15 13:48:52 -0400

    .

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import xmlrpc.client
+import sys
 
 proxy = xmlrpc.client.ServerProxy("http://localhost:6789")
 num1 = 12
@@ -6,3 +7,6 @@ num2 = 31
 
 result = proxy.testFunc(num1, num2)
 print("Result is:", result)
+
+print(sys.argv)
+proxy.updateCache(f'message is: {sys.argv[1]}')
--- server.py
+++ server.py
@@ -4,6 +4,14 @@ from xmlrpc.server import SimpleXMLRPCServer
 def testFunc(num1, num2):
 	return num1 + num2
 
+
+cache = []
+def updateCache(elem):
+	cache.append(elem)
+	print(cache)
+	return "message updated"
+
 server = SimpleXMLRPCServer(("localhost", 6789))
 server.register_function(testFunc, "testFunc")
+server.register_function(updateCache, "updateCache")
 server.serve_forever()
