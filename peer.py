cd55f648e66c980aa0c6c862b4f842cb4e8dad74
Chelsea Peragallo <Chelsea.Peragallo@ll.mit.edu>
2022-10-16 11:13:43 -0400

    changed peer to extend xmlserver class and added a kill function

--- peer.py
+++ peer.py
@@ -1,18 +1,31 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
 import threading
+import random
 
-class Peer:
+class Peer(SimpleXMLRPCServer):
+    #Peer class extends the capabilities of the simpleXMLRPCServer
+
     def __init__(self, name, addr, port) -> None:
+        super().__init__((addr, port), allow_none=True)
         self.msg = []
         self.name = name #(this could be role?)
+        
         # update proxy when connected to other server (neighbor)
         self.proxy = None
-        self.server = SimpleXMLRPCServer((addr, port), allow_none=True)
-        self.server.register_function(self.send, "send")
+        #self.server = SimpleXMLRPCServer((addr, port), allow_none=True)
+        self.register_function(self.send, "send")
+    
+    def serve_forever(self):
+    #modifies the server forever function in simplXMLRPCServer to handle requests forever
+    #until told to quit
+        self.quit = 0
+        while not self.quit:
+            self.handle_request()
+
     
     def startServer(self):
-        threading.Thread(target=self.server.serve_forever).start()
+        threading.Thread(target=self.serve_forever).start()
     
     # connect to another rpc server
     def connect(self, port):
@@ -23,6 +36,16 @@ class Peer:
     def send(self, message):
         self.msg.append(message)
         print(f"messgge from {message}")
+
+
+#-----------------------------------------------------------------
+    def setmyrole( self, assign_role = None ):
+        #decide which product and role a peer is by random assignment
+
+        if assign_role != None: #optional to assign roles to aid in testing
+            self.myrole = assign_role
+        else:
+            self.myrole = random.choice(['buyer','fish_seller','boar_seller','salt_seller'])
 
     # lookup by the buyer, terminate when hopCount = 0
     def lookup(self, buyerId, product_name:str, hopCount:int, path: list):
--- test.py
+++ test.py
@@ -1,29 +1,55 @@
 import peer
 import threading
 
+#Simple function to kill a server    
+def kill(server):
+    server.quit = 1
+    return 1
+
 # start server 
-p1 = peer.Peer('p1', "localhost", 8888)
+p1 = peer.Peer('p1', "localhost", 8890)
 # t = threading.Thread(target=p1.server.serve_forever)
 # t.start()
 p1.startServer()
 
 
-p2 = peer.Peer('p2', "localhost", 4444)
+p2 = peer.Peer('p2', "localhost", 4446)
 # t = threading.Thread(target=p2.server.serve_forever)
 # t.start()
 p2.startServer()
-p3 = peer.Peer("p3", "localhost", 5555)
+p3 = peer.Peer("p3", "localhost", 5557)
 
 # connect to port to send messages
-p1.connect(4444)
+p1.connect(4446)
 p1.proxy.send(f"hello from {p1.name}")
 
-p2.connect(8888)
+p2.connect(8890)
 p2.proxy.send(f"hello from {p2.name}")
 
 p1.proxy.send("I'm good!")
 print(p2.msg)
 
-p3.connect(4444)
+p3.connect(4446)
 p3.proxy.send("hi from peer 3")
 print(p2.msg)
+
+kill(p1)
+kill(p2)
+kill(p3)
+
+#-----------------------------------------------------------------
+# adding bazaar functionality to peer class
+# start server 
+buyer = peer.Peer('buyer', "localhost", 8020)
+# t = threading.Thread(target=p1.server.serve_forever)
+# t.start()
+buyer.setmyrole('buyer')
+print(buyer.myrole())
+#buyer.startServer()
+
+seller1 = peer.Peer('seller', "localhost", 8020)
+# t = threading.Thread(target=p1.server.serve_forever)
+# t.start()
+seller1.setmyrole()
+print(seller1.myrole())
+#seller1.startServer()
