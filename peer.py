4022022c45093c7b6cb0c9743ee2dbb814e04c55
nathanx98 <xuanjiyang09@gmail.com>
2022-10-16 21:23:12 -0400

    stuff

--- peer.py
+++ peer.py
@@ -1,20 +1,27 @@
-from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import MultiPathXMLRPCServer, SimpleXMLRPCServer
 import xmlrpc.client
 import threading
 import random
 import time
 import sys
+import multiprocessing
+from queue import Queue
+import os
+
 
 class Peer(): #Peer(SimpleXMLRPCServer):
     #Peer class extends the capabilities of the simpleXMLRPCServer
 
-    def __init__(self, name, serverport, peerlist=None,) -> None:
+    def __init__(self, name, serverport, myrole=None, peerlist=None,) -> None:
         #super().__init__((addr, serverport), allow_none=True)
-        self.msg = []
+        self.msg = Queue()
         self.name = name #pid or tuple(host,port)?
         self.locate = serverport #[addr,serverport]
-        self.myrole = None
+        self.myrole = myrole
         self.peerlist = peerlist
+        self.product_name = None
+
+
         # update proxy when connected to other server (neighbor)
         self.proxy = None
         self.server = SimpleXMLRPCServer(('localhost', serverport), allow_none=True)
@@ -29,12 +36,12 @@ class Peer(): #Peer(SimpleXMLRPCServer):
     
     # connect to another rpc server
     def connect(self, port):
-        print(f"http://localhost:{port}")
+        print(f"connecting to port {port}...")
         self.proxy = xmlrpc.client.ServerProxy(f"http://localhost:{port}")
 
     # send a message to proxy
     def send(self, message):
-        self.msg.append(message)
+        self.msg.put(message)
         print(f"message from {message}")
 
     def passMSG(self, destPort, message):
@@ -51,7 +58,7 @@ class Peer(): #Peer(SimpleXMLRPCServer):
             self.myrole = random.choice(['buyer', 'seller'])
 
         if self.myrole =='seller':
-            self.myproduct = random.choice(['salt', 'boar', 'fish'])#define what seller is selling
+            self.product_name = random.choice(['salt', 'boar', 'fish'])#define what seller is selling
             self.inventory = 5 #initialize number of items to sell
 
     def changeInventory(self):
@@ -66,18 +73,11 @@ class Peer(): #Peer(SimpleXMLRPCServer):
             pass
 
         myneighbors = self.peerlist[self.name]
-        hopCount = hopCount -1 #decrement hopcount now that we made a hop
+        hopCount = hopCount - 1 #decrement hopcount now that we made a hop
 
-        if self.myrole == 'buyer': #if I'm a buyer just pass the message along
+        # if i'm a buyer or doesn't sell that product, just pass the message
+        if self.myrole == 'buyer' or self.product_name != product_name:
             searchpath.append(self.locate) #add my location to the path
-
-            for n in range(len(myneighbors)):
-                neighbor = myneighbors[n]
-                self.connect(neighbor) #establish connection to my neighbor
-                self.proxy.lookup(product_name, hopCount, searchpath) #invoke the lookup function on my neighbor with the new path
-        
-        elif self.product_name != product_name: #I'm a seller but not for that item
-            searchpath = searchpath.append(self.locate) #add my location to the path
 
             for n in range(len(myneighbors)):
                 neighbor = myneighbors[n]
@@ -96,7 +96,6 @@ class Peer(): #Peer(SimpleXMLRPCServer):
 
         if len(path == 0):#we reached the buyer
             self.replyqueue[sellerId] = replypath #add to dictionary of replies
-        
         else:
             #call method recusivly on next peer
             nextpeer = reversed(path)[0] 
@@ -110,16 +109,16 @@ class Peer(): #Peer(SimpleXMLRPCServer):
 
         if len(path) == 0: #we reached the seller
             self.changeInventory() #update inventory
-            print(buyerId, " bought ", self.myproduct, " from ", self.name) 
+            print(buyerId, " bought ", self.product_name, " from ", self.name) 
         nextpeer = path[0] 
         self.connect(nextpeer)
         self.proxy.buy(buyerId, peerId, path)
 
 #-----------------------------------------------------------------
+    # mainloop only processes message queue
     def mainloop(self):
-        self.peerlist = self.peerlist
+        # self.peerlist = self.peerlist
         myneighbors = self.peerlist[self.name]
-
         if self.myrole == None:
             self.setmyrole() #peer decides which role it will be in the bazaar
             print(self.name, "role in the bazaar is", self.myrole)
@@ -132,11 +131,12 @@ class Peer(): #Peer(SimpleXMLRPCServer):
             print(self.name, "is a buyer, looking for", product_id)
             #send on to my neighbors by calling the lookup function
             for n in range(len(myneighbors)):
+                print(myneighbors)
                 searchpath = list()
                 self.lookup(product_id, 3, searchpath)
             
             print(self.name, "is a buyer, waiting...")
-            time.sleep(5)
+            time.sleep(2)
 
             #now parse out our reply queue, select one seller and buy the item
             print(self.replyqueue.keys(), "replied to", self.name)
@@ -144,17 +144,17 @@ class Peer(): #Peer(SimpleXMLRPCServer):
             self.buy(self.name, sellerId, path)
 
 if __name__ == "__main__":
-    peerlist = {'all':[9001,9002,9003,9004,9005,9006], 
-            'p1':[9002,9003], 'p2':[9004,9005], 
-            'p3':[9005,9006], 'p4':[9002,9005], 
-            'p5':[9002,9003,9004], 'p6':[9001,9003]}
+    # peerlist = {'all':[9001,9002,9003,9004,9005,9006], 
+    #         'p1':[9002,9003], 'p2':[9004,9005], 
+    #         'p3':[9005,9006], 'p4':[9002,9005], 
+    #         'p5':[9002,9003,9004], 'p6':[9001,9003]}
 
-    p = Peer(sys.argv[1], int(sys.argv[2]), peerlist)
-    
+    testNetwork ={'p1':[9002], 'p2':[9001]}
+    p = Peer(sys.argv[1], int(sys.argv[2]),sys.argv[3], testNetwork)
+    name = sys.argv[1]
+    p.startServer()
     time.sleep(5) #allow time for other peers to start
-    
+
     p.mainloop()
-    print("i finished my loop")
-
-
-
+    
+    print(f"{name} finished my loop")
--- test.py
+++ test.py
@@ -5,32 +5,32 @@ import subprocess
 import time
 
 # start server 
-p1 = peer.Peer('p1', 8891)
-# t = threading.Thread(target=p1.server.serve_forever)
-# t.start()
-p1.startServer()
+# p1 = peer.Peer('p1', 8891)
+# # t = threading.Thread(target=p1.server.serve_forever)
+# # t.start()
+# p1.startServer()
 
 
-p2 = peer.Peer('p2',4447)
-# t = threading.Thread(target=p2.server.serve_forever)
-# t.start()
-p2.startServer()
+# p2 = peer.Peer('p2',4447)
+# # t = threading.Thread(target=p2.server.serve_forever)
+# # t.start()
+# p2.startServer()
 
-# connect to port to send messages
-p1.connect(4447)
-p1.proxy.send(f"hello from {p1.name}")
+# # connect to port to send messages
+# p1.connect(4447)
+# p1.proxy.send(f"hello from {p1.name}")
 
 
-p2.connect(8891)
-p2.proxy.send(f"hello from {p2.name}")
+# p2.connect(8891)
+# p2.proxy.send(f"hello from {p2.name}")
 
-p1.proxy.send("I'm good!")
-print(p2.msg)
+# p1.proxy.send("I'm good!")
+# print(p2.msg)
 
-p3 = peer.Peer("p3", 5558)
-p3.connect(4447)
-p3.proxy.send("hi from peer 3")
-print(p2.msg)
+# p3 = peer.Peer("p3", 5558)
+# p3.connect(4447)
+# p3.proxy.send("hi from peer 3")
+# print(p2.msg)
 
 #-----------------------------------------------------------------
 # adding bazaar functionality to peer class
@@ -39,18 +39,18 @@ peerlist = {'all':[9001,9002,9003,9004,9005,9006],
             'p3':[9005,9006], 'p4':[9002,9005], 
             'p5':[9002,9003,9004], 'p6':[9001,9003]}
 
-subprocess.Popen(["python", "peer.py", "p1", "9001"])
+subprocess.Popen(["python3", "peer.py", "p1", "9001", 'buyer'])
 time.sleep(1)
-subprocess.Popen(["python", "peer.py", "p2", "9002"])
+subprocess.Popen(["python3", "peer.py", "p2", "9002", 'seller'])
 time.sleep(1)
-subprocess.Popen(["python", "peer.py", "p3", "9003"])
-time.sleep(1)
-subprocess.Popen(["python", "peer.py", "p4", "9004"])
-time.sleep(1)
-subprocess.Popen(["python", "peer.py", "p5", "9005"])
-time.sleep(1)
-subprocess.Popen(["python", "peer.py", "p6", "9006"])
-time.sleep(1)
+# subprocess.Popen(["python3", "peer.py", "p3", "9003"])
+# time.sleep(1)
+# subprocess.Popen(["python3", "peer.py", "p4", "9004"])
+# time.sleep(1)
+# subprocess.Popen(["python3", "peer.py", "p5", "9005"])
+# time.sleep(1)
+# subprocess.Popen(["python3", "peer.py", "p6", "9006"])
+# time.sleep(1)
 
 
 
@@ -60,45 +60,45 @@ time.sleep(1)
 
 
 
-# start server 
-buyer = peer.Peer('buyer', "localhost", 8002)
-# t = threading.Thread(target=p1.server.serve_forever)
-# t.start()
-buyer.setmyrole('buyer')
-print(buyer.myrole)
-#buyer.startServer()
+# # start server 
+# buyer = peer.Peer('buyer', "localhost", 8002)
+# # t = threading.Thread(target=p1.server.serve_forever)
+# # t.start()
+# buyer.setmyrole('buyer')
+# print(buyer.myrole)
+# #buyer.startServer()
 
-seller1 = peer.Peer('seller', "localhost", 8003)
-# t = threading.Thread(target=p1.server.serve_forever)
-# t.start()
-seller1.setmyrole('salt_seller')
-#seller1.startServer()
+# seller1 = peer.Peer('seller', "localhost", 8003)
+# # t = threading.Thread(target=p1.server.serve_forever)
+# # t.start()
+# seller1.setmyrole('salt_seller')
+# #seller1.startServer()
 
-p1 = peer.Peer('p1', "localhost", 8006)
-p1.startServer()
-p2 = peer.Peer('p2', "localhost", 8007)
-p2.startServer()
-p2.setmyrole('seller')
-p3 = peer.Peer('p2', "localhost", 8008)
-p3.startServer()
-p3.setmyrole('seller')
-p4 = peer.Peer('p2', "localhost", 8009)
-p4.startServer()
-p4.setmyrole('seller')
-p5 = peer.Peer('p2', "localhost", 8010)
-p5.startServer()
-p5.setmyrole('seller')
-p6 = peer.Peer('p2', "localhost", 8011)
-p6.startServer()
-p6.setmyrole('seller')
+# p1 = peer.Peer('p1', "localhost", 8006)
+# p1.startServer()
+# p2 = peer.Peer('p2', "localhost", 8007)
+# p2.startServer()
+# p2.setmyrole('seller')
+# p3 = peer.Peer('p2', "localhost", 8008)
+# p3.startServer()
+# p3.setmyrole('seller')
+# p4 = peer.Peer('p2', "localhost", 8009)
+# p4.startServer()
+# p4.setmyrole('seller')
+# p5 = peer.Peer('p2', "localhost", 8010)
+# p5.startServer()
+# p5.setmyrole('seller')
+# p6 = peer.Peer('p2', "localhost", 8011)
+# p6.startServer()
+# p6.setmyrole('seller')
 
-peerlist = {'all':[p1.locate,p2.locate,p3.locate,p4.locate,p5.locate,p6.locate], 
-            'p1':[p2.locate,p3.locate], 'p2':[p4.locate,p5.locate], 
-            'p3':[p5.locate,p6.locate], 'p4':[p2.locate,p5.locate], 
-            'p5':[p2.locate,p3.locate,p4.locate], 'p6':[p1.locate,p3.locate]}
-#test case p1 is a buyer and everyone else is a seller
-p1.setmyrole('buyer')
-p1.connect(8007)
-p1.proxy.send(f"hello from {p1.name}")
-#p1.proxy.buy('p1', 'p2', [p1.locate])
-#p1.mainloop(peerlist)
+# peerlist = {'all':[p1.locate,p2.locate,p3.locate,p4.locate,p5.locate,p6.locate], 
+#             'p1':[p2.locate,p3.locate], 'p2':[p4.locate,p5.locate], 
+#             'p3':[p5.locate,p6.locate], 'p4':[p2.locate,p5.locate], 
+#             'p5':[p2.locate,p3.locate,p4.locate], 'p6':[p1.locate,p3.locate]}
+# #test case p1 is a buyer and everyone else is a seller
+# p1.setmyrole('buyer')
+# p1.connect(8007)
+# p1.proxy.send(f"hello from {p1.name}")
+# #p1.proxy.buy('p1', 'p2', [p1.locate])
+# #p1.mainloop(peerlist)
